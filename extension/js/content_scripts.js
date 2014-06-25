2139b8e469de59086914080c20590b768cd68a9f
Kevin Tu <kevin.b.tu@gmail.com>
2014-06-25 10:45:12 -0700

    changed to look for classname not id on hover

--- extension/js/content_scripts.js
+++ extension/js/content_scripts.js
@@ -3,19 +3,21 @@
 ////////////////////////////////////////////////////////////////
 
 console.log('hello world');
+var g_classname = 'prettyprint';
 
 // Add bubble to the top of the page.
 var bubbleDOM = document.createElement('div');
 bubbleDOM.setAttribute('class', 'selection_bubble');
 document.body.appendChild(bubbleDOM);
 
-// Lets listen to mouseup DOM events.
+// listen to mouseup DOM events.
 document.addEventListener('mouseup', function (e) {
-console.log('mouseup');	
+	console.log('mouseup');	
   var selection = window.getSelection().toString();
-  if (selection.length > 0) {
-    renderBubble(e.clientX, e.clientY, selection);
-  }
+  console.log(selection);
+  // if (selection.length > 0) {
+  //   renderBubble(e.clientX, e.clientY, selection);
+  // }
 }, false);
 
 // Close the bubble when we click on the screen.
@@ -33,7 +35,15 @@ function renderBubble(mouseX, mouseY, selection) {
   bubbleDOM.style.left = (mouseX + left) + 'px';
   bubbleDOM.style.visibility = 'visible';
 }
-
+function processElement(class_list, e){
+	if(class_list.indexOf(g_classname) > -1){
+		renderBubble(e.clientX, e.clientY, "FOUND THE CLASS: " + g_classname);
+	}
+	else{
+		bubbleDOM.style.visibility = 'hidden';
+	}
+}
+// Be looking for ID's onmouseover
 document.onmouseover = function(e) {
     var targ;
     if (!e) var e = window.event;
@@ -41,8 +51,9 @@ document.onmouseover = function(e) {
     else if (e.srcElement) targ = e.srcElement;
     if (targ.nodeType == 3) // defeat Safari bug
         targ = targ.parentNode;
-    console.log(targ.id);
-    // CALL FUNCTION TO CHECK IF TARG.ID MATCHES THE ID OF OUR SPECIAL DOM'S 
+    // console.log(targ.id);
+    console.log(targ.className);
+    processElement(targ.className, e);     
 }
 
 
