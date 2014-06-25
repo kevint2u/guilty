b316d40486e49b60bec0aea1c238f93dde18a369
Kevin Tu <kevin.b.tu@gmail.com>
2014-06-25 10:20:15 -0700

    popup appears on select. console.log elem id on hover

--- extension/js/content_scripts.js
+++ extension/js/content_scripts.js
@@ -4,13 +4,44 @@
 
 console.log('hello world');
 
+// Add bubble to the top of the page.
+var bubbleDOM = document.createElement('div');
+bubbleDOM.setAttribute('class', 'selection_bubble');
+document.body.appendChild(bubbleDOM);
+
+// Lets listen to mouseup DOM events.
 document.addEventListener('mouseup', function (e) {
-  console.log('mouseup');
+console.log('mouseup');	
+  var selection = window.getSelection().toString();
+  if (selection.length > 0) {
+    renderBubble(e.clientX, e.clientY, selection);
+  }
 }, false);
 
+// Close the bubble when we click on the screen.
 document.addEventListener('mousedown', function (e) {
-  console.log('mousedown');
+	console.log('mousedown');
+  bubbleDOM.style.visibility = 'hidden';
 }, false);
+
+// Move that bubble to the appropriate location.
+function renderBubble(mouseX, mouseY, selection) {
+  bubbleDOM.innerHTML = selection;
+  bubbleDOM.style.top = mouseY + 'px';
+  bubbleDOM.style.left = mouseX + 'px';
+  bubbleDOM.style.visibility = 'visible';
+}
+
+document.onmouseover = function(e) {
+    var targ;
+    if (!e) var e = window.event;
+    if (e.target) targ = e.target;
+    else if (e.srcElement) targ = e.srcElement;
+    if (targ.nodeType == 3) // defeat Safari bug
+        targ = targ.parentNode;
+    console.log(targ.id);
+    // CALL FUNCTION TO CHECK IF TARG.ID MATCHES THE ID OF OUR SPECIAL DOM'S 
+}
 
 
 ////////////////////////////////////////////////////////////////
