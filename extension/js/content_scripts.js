42d93599b76acfe3135b85b6159597bad599c0b9
Kevin Tu <kevin.b.tu@gmail.com>
2014-06-25 09:26:44 -0700

    added content_scripts js file and to manifest. you can control actions on page through this js file

---
+++ extension/js/content_scripts.js
@@ -0,0 +1,9 @@
+console.log('hello world');
+
+document.addEventListener('mouseup', function (e) {
+  console.log('mouseup');
+}, false);
+
+document.addEventListener('mousedown', function (e) {
+  console.log('mousedown');
+}, false);
